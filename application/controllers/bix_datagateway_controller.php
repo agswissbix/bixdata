f2dcbe90e5e111624192ff4b16f0799ac686854c
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-02-03 17:41:09 +0100

    .

--- application/controllers/bix_datagateway_controller.php
+++ application/controllers/bix_datagateway_controller.php
@@ -189,7 +189,10 @@ class Bix_datagateway_controller extends CI_Controller {
         else
         {
             echo '<span style="color:red">INSERT RECORD</span> <br/>';
-            $fields['id']= $this->Sys_model->generate_id($tableid);
+            if($tableid!='Dipendenti')
+            {
+                $fields['id']= $this->Sys_model->generate_id($tableid);
+            }
             $recordid=$this->insert_record($tableid,1,$fields);
         }
         
