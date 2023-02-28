c1cafe3aed0843591174e640ee209508a9152c80
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-02-28 13:30:11 +0100

    .

--- application/controllers/api_controller.php
+++ application/controllers/api_controller.php
@@ -9,7 +9,8 @@ class Api_controller extends CI_Controller {
     
    public function create_project()
    {
-       
+        header("Access-Control-Allow-Methods: POST, GET");
+        header("Access-Control-Allow-Origin: *");
         $fields['id']= $this->Sys_model->generate_seriale('project', 'id');
         $fields['projectname']='test';
         $this->Sys_model->insert_record('project',1, $fields);
