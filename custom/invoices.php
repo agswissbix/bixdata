4cc6a950cc4e85ff6f1c2a94b922519a98931345
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-05-22 10:32:09 +0200

    .

--- custom/invoices.php
+++ custom/invoices.php
@@ -256,7 +256,7 @@ $invoices=$conn_bixdata->query($sql);
                                             if($invoice_row['type']=='Timesheet')
                                             {
                                             ?>
-                                                <button data-target="modal1" class="btn modal-trigger" onclick="$('.modal-content').find('iframe').attr('src','http://bixcrm01:8000/get_record_path/timesheet/<?=$invoice_row['timesheetid']?>/')">Apri</button>
+                                                <a href="http://bixcrm01:8000/get_record_path/timesheet/<?=$invoice_row['timesheetid']?>" target="blank_"><button data-target="modal1" class="btn modal-trigger" onclick="//$('.modal-content').find('iframe').attr('src','http://bixcrm01:8000/get_record_path/timesheet/<?=$invoice_row['timesheetid']?>/')">Apri</button></a>
                                             <?php
                                             }
                                             ?>    
