c50c41741ddac85b9ad0ae3332c9401d1d4f9eaa
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-07-14 11:42:02 +0200

    .

--- application/controllers/rest_controller.php
+++ application/controllers/rest_controller.php
@@ -121,10 +121,10 @@ class Rest_controller extends CI_Controller {
         $sql=$sql." $from WHERE $where  AND user_$table.deleted_<>'Y' ) AS risultati  ";
         $limit=50;
         $offset=$currentpage*$limit-$limit;
-        if(isempty($currentpage))
+        if($currentpage==0)
         {
             $offset=0;
-            $limit=1000000000000000000;
+            $limit=1000000000000000;
         }
         $return['records']=$this->Sys_model->get_records($table,$sql,$columns[3]['id'],'desc',$offset,$limit);
         
