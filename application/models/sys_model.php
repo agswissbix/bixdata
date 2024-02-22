d96548438d51b18a18a8035c4717e56a2b1dc4f7
agswissbix <108857221+agswissbix@users.noreply.github.com>
2024-02-22 09:30:37 +0100

    .

--- application/models/sys_model.php
+++ application/models/sys_model.php
@@ -10489,7 +10489,7 @@ class Sys_model extends CI_Model {
              {
                  $sql="SELECT recordid_,$keyfieldlink FROM user_$mastertableid WHERE $keyfield_condition ORDER BY priorityid desc,name asc LIMIT 30";
              }
-             if($mastertableid=='compansdfy')
+             if($mastertableid=='company')
              {
                  $sql="SELECT recordid_,$keyfieldlink FROM user_$mastertableid WHERE $keyfield_condition AND ((bexio_status!='Deleted' AND bexio_contact_type_id!='2') OR id_bexio is null) ORDER BY recordid_ desc LIMIT 20";
              }
